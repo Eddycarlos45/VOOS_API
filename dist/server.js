3de9d81637f9afbc83d3e95ff2a300b15a43238b
edsonsousa33 <edson.sousa@compasso.com.br>
2021-11-22 18:02:06 +0000

    build

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var custom_express_1 = require("./config/custom_express");
-custom_express_1.app.listen(3000, function () { return console.log('Server is running'); });
+custom_express_1.app.listen(process.env.PORT, function () { return console.log("Server is running on port " + process.env.PORT); });
